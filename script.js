0371389e9926356e4867ee325245a42d23731931
Shaik Shasha Vali <shashavali8524@gmail.com>
2025-11-26 18:58:41 +0530

    k

--- script.js
+++ script.js
@@ -402,14 +402,7 @@ document.addEventListener("focusin", (e) => {
 });
 
 // Prevent zoom on double tap for mobile
-let lastTouchEnd = 0;
-document.addEventListener("touchend", (e) => {
-  const now = Date.now();
-  if (now - lastTouchEnd <= 300) {
-    e.preventDefault();
-  }
-  lastTouchEnd = now;
-}, false);
+
 
 function addLetter(l) {
   if (!guesses[currentRow]) guesses[currentRow] = "";
